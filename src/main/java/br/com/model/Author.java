6b78c2de4bbd7bcebcad4cb1b0ca0063776c381d
rwfazul <rwfazul@inf.ufsm>
2018-06-01 19:06:46 -0300

    change to abstract class for xsi:type definition

--- src/main/java/br/com/model/Author.java
+++ src/main/java/br/com/model/Author.java
@@ -8,24 +8,65 @@ package br.com.model;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author isabella
+ * @author rhau
  */
+@XmlTransient
+@XmlSeeAlso(value = {Application_t.class}) 
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Author {
-    
+public abstract class Author {
+
     @XmlElement(name = "Name")
-    String name;
-    
-    @XmlElement(name = "LangID")
-    String langID;
-    
-    @XmlElement(name = "PartNumber")
-    String partNumber;
+    private String name;
     
     @XmlElement(name = "Build")
-    Build build;
+    private Build build;
+        
+    @XmlElement(name = "LangID")
+    private String langID;
+    
+    @XmlElement(name = "PartNumber")
+    private String partNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Build getBuild() {
+        return build;
+    }
+
+    public void setBuild(Build build) {
+        this.build = build;
+    }
+
+    public String getLangID() {
+        return langID;
+    }
+
+    public void setLangID(String langID) {
+        this.langID = langID;
+    }
+
+    public String getPartNumber() {
+        return partNumber;
+    }
+
+    public void setPartNumber(String partNumber) {
+        this.partNumber = partNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" + ", name=" + name + ", build=" + build + ", langID=" + langID + ", partNumber=" + partNumber + '}';
+    }
     
 }
--- src/main/java/br/com/model/Creator.java
+++ src/main/java/br/com/model/Creator.java
@@ -7,28 +7,66 @@ package br.com.model;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
  * @author isabella
  */
+@XmlTransient
+@XmlSeeAlso(value = {Device_t.class})
 @XmlAccessorType(XmlAccessType.FIELD)
-class Creator {
-    
-    @XmlAttribute(name = "xsi:type")
-    String xsitype;
+public abstract class Creator {
     
     @XmlElement(name = "Name")
-    String name;
+    private String name;
     
     @XmlElement(name = "UnitId")
-    Integer unitId;
+    private Integer unitId;
     
-    @XmlElement(name = "ProductId")
-    Integer productId;
+    @XmlElement(name = "ProductID")
+    private Integer productId;
     
     @XmlElement(name = "Version")
-    Version version;
+    private Version version;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(Integer unitId) {
+        this.unitId = unitId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Version getVersion() {
+        return version;
+    }
+
+    public void setVersion(Version version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "Creator{" + ", name=" + name + ", unitId=" + unitId + ", productId=" + productId + ", version=" + version + '}';
+    }
+
 }
