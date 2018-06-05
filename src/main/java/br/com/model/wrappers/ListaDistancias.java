e2c000a29a06789ba11b5748f1f3deef51a24721
rwfazul <rwfazul@inf.ufsm>
2018-06-05 11:08:56 -0300

    mv wrappers classes to model directory

--- src/main/java/br/com/model/wrappers/ListaDistancias.java
+++ src/main/java/br/com/model/wrappers/ListaDistancias.java
@@ -3,12 +3,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.ws;
+package br.com.model.wrappers;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
  * @author rhau
  */
-class ListaDouble {
+@XmlRootElement(name = "distanciasLaps")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ListaDistancias {
     
+    @XmlElement(name = "distancia")
+    private List<Double> distancias = new ArrayList<>();
+    
+    public ListaDistancias() {
+    }
+
+    public List<Double> getDistancias() {
+        return distancias;
+    }
+
+    public void setDistancias(List<Double> distancias) {
+        this.distancias = distancias;
+    }
+
+    @Override
+    public String toString() {
+        return "ListaDouble{" + "distancias=" + distancias + '}';
+    }
+       
 }
--- src/main/java/br/com/model/wrappers/ListaDuracoes.java
+++ src/main/java/br/com/model/wrappers/ListaDuracoes.java
@@ -3,12 +3,56 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.ws;
+package br.com.model.wrappers;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
  * @author rhau
  */
-class ListaDate {
+@XmlRootElement(name = "duracoesLaps")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ListaDuracoes {
+ 
+    @XmlAttribute(name = "formato")
+    private String formato;
+    
+    @XmlElement(name = "duracao")
+    private List<String> duracoes = new ArrayList<>();
+
+    public ListaDuracoes(String formato) {
+        this.formato = formato;
+    }
+    
+    public ListaDuracoes() {
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public List<String> getDuracoes() {
+        return duracoes;
+    }
+
+    public void setDuracoes(List<String> duracoes) {
+        this.duracoes = duracoes;
+    }
+
+    @Override
+    public String toString() {
+        return "ListaDuracoes{" + "formato=" + formato + ", duracoes=" + duracoes + '}';
+    }
     
 }
